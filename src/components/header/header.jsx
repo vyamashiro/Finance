3b46e14888ed607ef074e265fb7a3ddc4de04990
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2021-08-28 23:18:41 -0300

    add logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Container } from './header.styles';
-
+import { Container, Logo } from './header.styles';
+import logo from './logo.png';
 
 export default function Header() {
     return(
         <>
             <Container>
+                <Logo src={logo} alt="logo da empresa com o nome Fliper" />
             </Container>
         </>
     );
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -3,8 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.header`
     background: linear-gradient(90deg,#3b5cb8,#6d9fff);
     left: 0;
-    height: 50px;
+    height: 60px;
     position: fixed;
     top: 0;
     width: 100%;
+`
+export const Logo = styled.img`
+    height: 35px;
+    margin: 10px;
+    width: 100px;
 `
