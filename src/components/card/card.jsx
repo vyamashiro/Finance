d331bde48f4b9dbe45fb171adbd3fa984c0dd6ca
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2021-08-30 23:05:42 -0300

    ajust

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Header, MoreOptionButton, Main, H3, Ol, Li, Value, Footer, ButtonMoreOptions } from './card.styles';
 import IconMoreOptions from '../../assets/IconMoreOptions';
 import { useQuery } from '@apollo/client';
 import WEALTH_SUMMARY from '../../querys/querys';
 
 export default function Card() {
-    const { loading, error, data } = useQuery(WEALTH_SUMMARY);
+    const [isVisible, setIsVisible] = useState(true)
+    const { loading, error, data  } = useQuery(WEALTH_SUMMARY);
 
-    if (loading) {
-        return <p>Carregando...</p>;
-      }
-    
-    if (error) {
-        return <p>ocorreu um erro, tente novamente mais tarde...</p>;
+    if (loading) { return <p>Carregando...</p>;}
+    if (error) { return <p>Ocorreu um erro, tente novamente mais tarde.</p>;}
+
+    const handleVisibility = (e) => { 
+        e.preventDefault();
+        setIsVisible(!isVisible)
     }
 
     return(
@@ -21,17 +22,17 @@ export default function Card() {
                 <Container key={index}>
                     <Header>
                         <h1>Investimento {info.id}</h1>
-                        <MoreOptionButton>
+                        <MoreOptionButton onClick={handleVisibility}>
                             <IconMoreOptions />
                         </MoreOptionButton>
                     </Header>
                     <Main>
                         <span>Valor investido</span>
-                        <H3>{info.total.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</H3>
+                        <H3>{isVisible ? info.total.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}) : '---'}</H3>
                         <Ol>
-                            <Li>Rentabilidade<Value>{info.profitability.toFixed(2) + '%'}</Value></Li>
-                            <Li>CDI<Value>{info.cdi.toFixed(2) + '%'}</Value></Li>
-                            <Li>Ganho/mês<Value>{info.gain.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</Value></Li>
+                            <Li>Rentabilidade<Value>{isVisible ? info.profitability.toFixed(2) + '%' : '---'}</Value></Li>
+                            <Li>CDI<Value>{isVisible ? info.cdi.toFixed(2) + '%' : '---'}</Value></Li>
+                            <Li>Ganho/mês<Value>{isVisible ? info.gain.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}) : '---'}</Value></Li>
                         </Ol>
                     </Main>
                     <Footer>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Container, Logo } from './header.styles';
-import logo from './logo.png';
+import logo from '../../assets/logo.png';
 
 export default function Header() {
+    
+    const valuesAreVisibies = () => {console.log('ok')}
+
     return(
         <>
             <Container>
                 <Logo src={logo} alt="logo da empresa com o nome Fliper" />
+                <button onClick={valuesAreVisibies}>botão</button>
             </Container>
         </>
     );
