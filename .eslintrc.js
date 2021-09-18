15a93b77e3f1752d86e52b62b332c7e750617f57
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2021-09-18 00:39:54 -0300

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      tsx: true,
     },
     ecmaVersion: 12,
     sourceType: 'module',
